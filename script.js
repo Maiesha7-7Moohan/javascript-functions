421602a17cf45051edd11dfc181d63b453b6bdc1
Maiesha Moohan <maieshamoohan@gmail.com>
2026-05-26 15:16:14 +0200

    initial commit

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function calculateScore(testOne, testTwo) {
 }
 
 function showResult(name, score) {
-  message.textContent = `${name}, your total score is ${score}`;
+  message.textContent = `${name}, your total score is ${score}.`;
 }
 
 button.addEventListener("click", function () {
   const studentName = prompt("What is your name?");
-  const firstScore = Number(prompt("Enter first score:"));
-  const secondScore = Number(prompt("Enter second score:"));
+  const firstScore = Number(prompt("Enter first score"));
+  const secondScore = Number(prompt("Enter second score"));
 
   const finalScore = calculateScore(firstScore, secondScore);
   showResult(studentName, finalScore);
